276f1fb675f3161144b353f6d5d5e81da1865088
floribe2000 <43866194+floribe2000@users.noreply.github.com>
2021-08-25 15:58:51 +0200

    Handle different skill modifier formats for captains

--- DataConverter/WGStructure/WGCommon.cs
+++ DataConverter/WGStructure/WGCommon.cs
@@ -7,4 +7,14 @@
         public string type { get; set; }
     }
 
+    public class ModifiersPerClass
+    {
+        public float AirCarrier { get; set; }
+        public float Auxiliary { get; set; }
+        public float Battleship { get; set; }
+        public float Cruiser { get; set; }
+        public float Destroyer { get; set; }
+        public float Submarine { get; set; }
+    }
+
 }
