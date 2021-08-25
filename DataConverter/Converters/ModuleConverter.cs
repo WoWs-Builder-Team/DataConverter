afc00a6dc836f644a5f85f4a51bb70253a555de7
iTTou <perotti.denis@gmail.com>
2021-08-25 19:52:39 +0200

    Created ModuleConverter

---
+++ DataConverter/Converters/ModuleConverter.cs
@@ -0,0 +1,43 @@
+﻿using DataConverter.WGStructure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using WoWsShipBuilderDataStructures;
+
+namespace DataConverter.Converters
+{
+    public class ModuleConverter
+    {
+        //convert the list of modernizations from WG to our list of Modernizations
+        public static Dictionary<string, Module> ConvertModule(string jsonInput)
+        {
+            //create a List of our Objects
+            Dictionary<string, Module> moduleList = new Dictionary<string, Module>();
+
+            //deserialize into an object
+            var wgModules = JsonConvert.DeserializeObject<List<WGModule>>(jsonInput) ?? throw new InvalidOperationException();
+
+            //iterate over the entire list to convert everything
+            foreach (var currentWgModule in wgModules)
+            {
+                //create our object type
+                Module module = new Module
+                {
+                    //start mapping
+                    CostCr = currentWgModule.costCR,
+                    CostXp = currentWgModule.costXP,
+                    Id = currentWgModule.id,
+                    Index = currentWgModule.index,
+                    Name = currentWgModule.name
+                };
+                //for Type enum
+                ModuleType type = (ModuleType)Enum.Parse(typeof(ModuleType), currentWgModule.typeinfo.species);
+                module.Type = type;
+                //dictionary with Index as key
+                moduleList.Add(module.Index, module);
+            }
+
+            return moduleList;
+        }
+    }
+}
--- WoWsShipBuilderDataStructures/Module.cs
+++ WoWsShipBuilderDataStructures/Module.cs
@@ -5,6 +5,7 @@
         public int CostCr { get; set; }
         public int CostXp { get; set; }
         public long Id { get; set; }
+        public string Index { get; set; }
         public string Name { get; set; }
         public ModuleType Type { get; set; }
     }
