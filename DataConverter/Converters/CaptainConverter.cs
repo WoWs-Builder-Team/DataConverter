821d88a135ffbfba78c6747a6e40ceebc9595e7b
iTTou <perotti.denis@gmail.com>
2021-08-27 15:53:26 +0200

    Completed CaptainConverter. Added SkillsTiers class to deserialize skills' tiers. Added missing properties to Captain

--- DataConverter/Converters/CaptainConverter.cs
+++ DataConverter/Converters/CaptainConverter.cs
@@ -9,13 +9,17 @@ namespace DataConverter.Converters
     public static class CaptainConverter
     {
         //convert the list of captains from WG to our list of Captains
-        public static Dictionary<string, Captain> ConvertCaptain(string jsonInput)
+        //jsonCaptainInput contains all Captains' informations
+        //jsonSkillsInput contains all skills' tiers for all the classes
+        public static Dictionary<string, Captain> ConvertCaptain(string captainJsonInput, string skillsJsonInput)
         {
             //create a List of our Objects
             Dictionary<string, Captain> captainList = new Dictionary<string, Captain>();
 
             //deserialize into an object
-            var wgCaptain = JsonConvert.DeserializeObject<List<WGCaptain>>(jsonInput) ?? throw new InvalidOperationException();
+            var wgCaptain = JsonConvert.DeserializeObject<List<WGCaptain>>(captainJsonInput) ?? throw new InvalidOperationException();
+
+            var skillsTiers = JsonConvert.DeserializeObject<SkillsTiers>(skillsJsonInput) ?? throw new InvalidOperationException();
 
             //iterate over the entire list to convert everything
             foreach (var currentWgCaptain in wgCaptain)
@@ -37,10 +41,11 @@ namespace DataConverter.Converters
 
                 //create object SKill
                 Skill skill = new Skill();
-                //initialize dictionaries for skills and skill's modifiers
+                //initialize dictionaries for skills, skill's modifiers and skill's tiers
                 Dictionary<string, Skill> skills = new Dictionary<string, Skill>();
                 Dictionary<string, float> modifiers = new Dictionary<string, float>();
                 Dictionary<string, float> conditionalModifiers = new Dictionary<string, float>();
+                Dictionary<ShipClass, int> tiers = new Dictionary<ShipClass, int>();
 
                 //iterate all captain's skills
                 foreach (var currentWgSkill in currentWgCaptain.Skills)
@@ -50,7 +55,36 @@ namespace DataConverter.Converters
                     skill.IsEpic = currentWgSkill.Value.isEpic;
                     skill.SkillNumber = currentWgSkill.Value.skillType;
 
-                    //collect all skill's modifiers
+                    //map skill's tier for each class
+                    if (skillsTiers.Cruiser.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.Cruiser, skillsTiers.Cruiser[skill.SkillNumber]);
+                    }
+                    if (skillsTiers.Auxiliary.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.Auxiliary, skillsTiers.Auxiliary[skill.SkillNumber]);
+                    }
+                    if (skillsTiers.Destroyer.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.Destroyer, skillsTiers.Destroyer[skill.SkillNumber]);
+                    }
+                    if (skillsTiers.AirCarrier.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.AirCarrier, skillsTiers.AirCarrier[skill.SkillNumber]);
+                    }
+                    if (skillsTiers.Submarine.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.Submarine, skillsTiers.Submarine[skill.SkillNumber]);
+                    }
+                    if (skillsTiers.Battleship.ContainsKey(skill.SkillNumber))
+                    {
+                        tiers.Add(ShipClass.Battleship, skillsTiers.Battleship[skill.SkillNumber]);
+                    }
+                    skill.Tiers = tiers;
+                    //list of the classes that can use the skill
+                    skill.LearnableOn = new List<ShipClass>(tiers.Keys);
+
+                    //collect all modifiers of the skill
                     foreach (var currentWgModifier in currentWgSkill.Value.modifiers)
                     {
                         modifiers.Add(currentWgModifier.Key, (float)currentWgModifier.Value);
---
+++ WoWsShipBuilderDataStructures/SkillsTiers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class SkillsTiers // collection of all the skills and their tier for each class
+{
+    public Dictionary<int, int> Cruiser { get; set; }
+    public Dictionary<int, int> Auxiliary { get; set; }
+    public Dictionary<int, int> Destroyer { get; set; }
+    public Dictionary<int, int> AirCarrier { get; set; }
+    public Dictionary<int, int> Submarine { get; set; }
+    public Dictionary<int, int> Battleship { get; set; }
+}
