aa9adbf14056e8aae94beb85303a31f5716b9c13
floribe2000 <git@floribe2000.de>
2022-09-25 00:02:21 +0200

    Move Aircraft.cs to Aircraft directory and reformat file

--- DataConverter/Converters/AircraftConverter.cs
+++ DataConverter/Converters/AircraftConverter.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using DataConverter.Data;
 using WoWsShipBuilder.DataStructures;
+using WoWsShipBuilder.DataStructures.Aircraft;
 using WowsShipBuilder.GameParamsExtractor.WGStructure;
 
 namespace DataConverter.Converters
