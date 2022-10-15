d94265ebe4fcde314b4235c4ace54d372e25eef9
floribe2000 <git@floribe2000.de>
2022-10-16 01:06:13 +0200

    Add extension function to combine anti-air auras

--- DataConverter/ConverterExtensions.cs
+++ DataConverter/ConverterExtensions.cs
@@ -103,24 +103,4 @@ public static class ConverterExtensions
         HorizontalSector = wgTorpedoLauncher.HorizSector,
         TorpedoAngles = wgTorpedoLauncher.TorpedoAngles,
     };
-
-    public static AntiAirAura AddAura(this AntiAirAura first, AntiAirAura second)
-    {
-        if (first.MaxRange > 0 && (first.MaxRange != second.MaxRange || first.HitChance != second.HitChance))
-        {
-            throw new InvalidOperationException("Cannot combine auras with different ranges or accuracies.");
-        }
-
-        decimal minRange = second.FlakDamage > 0 ? first.MinRange : second.MinRange; // Use minimum range of new aura only if it is no flak cloud aura
-
-        return new()
-        {
-            ConstantDps = first.ConstantDps + second.ConstantDps,
-            FlakDamage = first.FlakDamage + second.FlakDamage,
-            FlakCloudsNumber = first.FlakCloudsNumber + second.FlakCloudsNumber,
-            HitChance = second.HitChance,
-            MaxRange = second.MaxRange,
-            MinRange = minRange,
-        };
-    }
 }
---
+++ WoWsShipBuilder.DataStructures/DataExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WoWsShipBuilder.DataStructures.Ship;
+
+namespace WoWsShipBuilder.DataStructures;
+
+public static class DataExtensions
+{
+    public static AntiAirAura AddAura(this AntiAirAura first, AntiAirAura second)
+    {
+        if (first.MaxRange > 0 && (first.MaxRange != second.MaxRange || first.HitChance != second.HitChance))
+        {
+            throw new InvalidOperationException("Cannot combine auras with different ranges or accuracies.");
+        }
+
+        decimal minRange = second.FlakDamage > 0 ? first.MinRange : second.MinRange; // Use minimum range of new aura only if it is no flak cloud aura
+
+        return new()
+        {
+            ConstantDps = first.ConstantDps + second.ConstantDps,
+            FlakDamage = first.FlakDamage + second.FlakDamage,
+            FlakCloudsNumber = first.FlakCloudsNumber + second.FlakCloudsNumber,
+            HitChance = second.HitChance,
+            MaxRange = second.MaxRange,
+            MinRange = minRange,
+        };
+    }
+}
