275f154eaa29a49cb52fb496f4db591619ffbcce
floribe2000 <git@floribe2000.de>
2023-04-07 17:11:53 +0200

    add logging to data downloader

--- DataConverter.Console/Services/DataDownloader.cs
+++ DataConverter.Console/Services/DataDownloader.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Compression;
+using Microsoft.Extensions.Logging;
 
 namespace DataConverter.Console.Services;
 
@@ -8,16 +9,45 @@ namespace DataConverter.Console.Services;
 public class DataDownloader
 {
     private readonly HttpClient httpClient;
+    private readonly ILogger<DataDownloader> logger;
 
-    public DataDownloader(HttpClient httpClient)
+    public DataDownloader(HttpClient httpClient, ILogger<DataDownloader> logger)
     {
         this.httpClient = httpClient;
+        this.logger = logger;
     }
 
     public async Task DownloadGameData(string dataUrl, string targetDirectory)
     {
+        logger.LogInformation("Preparing game data download from {dataUrl} to {targetDirectory}", dataUrl, targetDirectory);
+        PrepareTargetDirectory(targetDirectory);
+        logger.LogDebug("Directory prepared, downloading data");
         var zipStream = await httpClient.GetStreamAsync(dataUrl);
         var archive = new ZipArchive(zipStream);
         archive.ExtractToDirectory(targetDirectory);
+        logger.LogInformation("Game data downloaded and extracted to {targetDirectory}", targetDirectory);
+    }
+
+    private void PrepareTargetDirectory(string targetDirectory)
+    {
+        var di = new DirectoryInfo(targetDirectory);
+        if (!di.Exists)
+        {
+            logger.LogInformation("Target directory does not exist, creating it.");
+            Directory.CreateDirectory(targetDirectory);
+            return;
+        }
+
+        logger.LogInformation("Target directory exists, deleting all files and subdirectories.");
+        foreach (var directoryInfo in di.GetDirectories())
+        {
+            logger.LogDebug("Deleting directory {}", directoryInfo.FullName);
+            directoryInfo.Delete(true);
+        }
+        foreach (var fileInfo in di.GetFiles())
+        {
+            logger.LogDebug("Deleting file {}", fileInfo.FullName);
+            fileInfo.Delete();
+        }
     }
 }
