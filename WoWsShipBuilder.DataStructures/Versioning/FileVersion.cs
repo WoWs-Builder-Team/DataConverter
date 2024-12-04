2ccee4123a2f6babb555ebc304811e74ade9523a
floribe2000 <git@floribe2000.de>
2024-12-04 22:23:31 +0100

    update to dotnet 9

--- WoWsShipBuilder.DataStructures/Versioning/FileVersion.cs
+++ WoWsShipBuilder.DataStructures/Versioning/FileVersion.cs
@@ -12,11 +12,11 @@ public sealed record FileVersion(string FileName, int Version, string Checksum =
     public static string ComputeChecksum(Stream stream)
     {
         using var sha256 = SHA256.Create();
-        return BitConverter.ToString(sha256.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+        return Convert.ToHexStringLower(sha256.ComputeHash(stream));
     }
 
     public static string ComputeChecksum(string data)
     {
-        return BitConverter.ToString(SHA256.HashData(Encoding.UTF8.GetBytes(data))).Replace("-", "").ToLowerInvariant();
+        return Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(data)));
     }
 }
